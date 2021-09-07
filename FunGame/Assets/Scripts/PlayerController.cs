1a4fadb48e89d8c2756613864b5d8b77a2e2aec4
Ben Arno <bdarno92@gmail.com>
2021-09-07 10:51:46 -0700

    mans is jumpin

---
+++ FunGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
+
+  private Rigidbody _playerRb;
+  // Start is called before the first frame update
+  void Start()
+  {
+    _playerRb = GetComponent<Rigidbody>();
+    Physics.gravity *= gravityModifier;
+        isOnGround = true;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      _playerRb.AddForce(Vector3.up * jumpForce);
+    }
+  }
+
+  private void OnCollision()
+  {
+    isOnGround = true;
+  }
+}
